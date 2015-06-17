a562ae17f613831ab6b88a7c9a2c6be75ef34e87
zaifeng <20012382@qq.com>
2015-06-17 17:57:27 +0800

    modify config

--- public/index.php
+++ public/index.php
@@ -1,6 +1,8 @@
 <?php
+//define env variable
 define('APP_ROOT' , dirname(__DIR__) );
 define('APP_PATH' , APP_ROOT.'/application');
-$app  = new Yaf_Application(APP_ROOT . "/conf/app.ini");
+define('APP_CONF' , APP_ROOT.'/conf');
+$app  = new Yaf_Application(APP_CONF . "/app.ini",'product');
 $app->bootstrap()
     ->run();
