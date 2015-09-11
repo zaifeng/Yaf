9d5afc87c5c2f034fc340c211c36f20f53a23f80
zaifeng <20012382@qq.com>
2015-09-11 16:29:13 +0800

    dividual controller and actions

---
+++ application/actions/member/Test.php
@@ -0,0 +1,9 @@
+<?php
+
+class TestAction extends Yaf_Action_Abstract
+{
+    public function execute()
+    {
+        echo "Hello World" ;
+    }
+} 
--- application/controllers/App.php
+++ application/controllers/App.php
@@ -7,5 +7,10 @@ class AppController extends Yaf_Controller_Abstract
     public function init()
     {
         $this->getView()->config = Yaf_Application::app()->getConfig();
+        Yaf_Dispatcher::getInstance()->disableView();
     }
+
+    public $actions = array(
+        'test' => 'actions/member/Test.php',
+    );
 }
